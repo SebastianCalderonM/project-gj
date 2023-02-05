00cd1bdc8347dfdb217b301f6c797c919010d803
Luis Corrales <luis.corrales@sansano.usm.cl>
2023-02-05 18:02:52 -0300

    Agregar cooldown a los ataques del jugador
    Defini tasas de disparo o golpe melee, para que no puedan spamear los ataques, en el caso del ataque melee calza justo con la animacion. En caso de querer redefinir o modificar estos cooldown lo pueden hacer desde el inspector, ya que son variables publicas.

--- GameJam/Assets/Scripts/player/ataques_jugador.cs
+++ GameJam/Assets/Scripts/player/ataques_jugador.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class ataques_jugador : MonoBehaviour
 {
 
-   public Transform firePosition;
-   public GameObject proyectil;
+    public Transform firePosition;
+    public GameObject proyectil;
 
-   public Transform golpePosition;
-   public GameObject golpe;
+    public Transform golpePosition;
+    public GameObject golpe;
+    public float golpeRate = 2f;
+    private float nextGolpe = 0f;
+    public float fireRate = 0.5f;
+    private float nextFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1)) // disparar
-        {   
+        if (Input.GetMouseButtonDown(1) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
             Instantiate(proyectil, firePosition.position, firePosition.rotation);
         }
 
-        if (Input.GetMouseButtonDown(0)) // ataque melee
+        if (Input.GetMouseButtonDown(0) && Time.time > nextGolpe)
         {
+            nextGolpe = Time.time + golpeRate;
             Instantiate(golpe, golpePosition.position, golpePosition.rotation, transform);
         }
     }
