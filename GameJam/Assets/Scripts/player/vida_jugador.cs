c073398c32c41dd553b9cbeb63cb64e1709167b7
Luis Corrales <luis.corrales@sansano.usm.cl>
2023-02-05 18:04:59 -0300

    Arregle error comentando el animador (no se usaba)
    Comente las lineas que tenian que ver con el animator controller que estuvieron probando, ya que tiraba un error.

--- GameJam/Assets/Scripts/player/vida_jugador.cs
+++ GameJam/Assets/Scripts/player/vida_jugador.cs
@@ -7,7 +7,7 @@ public class vida_jugador : MonoBehaviour
 {
     public GameOver gameOver;
     public int health = 100;
-    public Animator animo;
+    //public Animator animo;
 
     public Slider healthBar;
     private void Update()
@@ -26,9 +26,9 @@ public class vida_jugador : MonoBehaviour
     {
         // Lógica para matar al jugador
         Debug.Log("Player died");
-        animo.SetTrigger("1");
+        //animo.SetTrigger("1");
         //yield return new WaitForSeconds(4);
-        animo.SetTrigger("2");
+        //animo.SetTrigger("2");
         //Destroy(gameObject);
         gameOver.GOver();
 
