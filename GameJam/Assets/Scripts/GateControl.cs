b2b75d02fd9f51fe2807b93227bcd6d0b1d049fe
DanQ12 <danirodrigo95@gmail.com>
2023-02-05 16:20:00 -0300

    Spawn enemigos en las habitaciones

--- GameJam/Assets/Scripts/GateControl.cs
+++ GameJam/Assets/Scripts/GateControl.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class GateControl : MonoBehaviour
 {
     public GameObject[] gates;
+    public GameObject[] enemy;
+    private GameObject[] enemyCount;
+
+    private bool isActivated = false;
+
+    public int enemyNumber;
+    private int enemyType;
+
+    public   Vector3[] spawnPoints = new Vector3[7];
+
+    private void Update()
+    {
+        if(isActivated == true)
+        {
+            enemyCount = GameObject.FindGameObjectsWithTag("Enemy");
+
+            if(enemyCount.Length == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) {
+        if (collision.gameObject.CompareTag("Player") && isActivated == false) {
             gates[0].SetActive(true);
 
             if( gates.Length > 1)
@@ -16,7 +38,15 @@ public class GateControl : MonoBehaviour
                 gates[1].SetActive(true);
             }
 
-            Destroy(gameObject, 4f);
+            enemyNumber = Random.Range(3, 8);
+
+            for (int i = 0; i < enemyNumber; i++) {
+
+                enemyType = Random.Range(0, enemy.Length);
+                Instantiate(enemy[enemyType], transform.position + spawnPoints[i], Quaternion.identity);
+            }
+
+            isActivated = true;
         }
     }
 }
